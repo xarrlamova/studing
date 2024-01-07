92e86f8f2f321f7e02fda6c3380129b40ada146f
Алиса Харламова <xarlamova_alisa1@outlook.com>
2024-01-07 16:22:08 +0700

    Добавлены новые компоненты, стили

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import {Avatar} from "./Components/Avatar";
 import UserInfo from "./Components/UserInfo";
 import User from "./Components/User";
 import Friends from "./Components/Friends";
-
+import AddUser from "./Components/AddFriend";
+// import { IoCreateOutline } from "react-icons/io5"; //редактировать
+// import { IoClose } from "react-icons/io5"; //закрыть
+// <IoClose />
+// import { IoCheckmark } from "react-icons/io5"; //галочка
+// <IoCheckmark />
 function App() {
+    console.log('234')
   return (
       <div>
           <Avatar />
           <UserInfo name={User().name} lastName={User().lastname}/>
-          <div className="addFriends"></div>
+
+          <div className="addFriends"><AddUser /></div>
           <div className='friends'>
               <Friends />
           </div>
