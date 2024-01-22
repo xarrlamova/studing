7d85e9385df45e1cc2f2104ff35c9d1a9394ccda
Алиса Харламова <xarlamova_alisa1@outlook.com>
2024-01-22 20:06:53 +0700

    Добавлена валидация на все формы.

--- src/Components/AddFriend.js
+++ src/Components/AddFriend.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useEffect} from "react";
 
 export const AddFriend = props => {
     const {onAdd} = props;
@@ -20,29 +21,61 @@ export const AddFriend = props => {
 
     const [nameError, setNameError] = useState('поле Имя не может быть пустым')
     const [fNameError, setFNameError] = useState('поле Фамилия не может быть пустым')
-    const [iAgeError, setIAgeError] = useState('поле озраст не может быть  пустым')
+    const [iAgeError, setIAgeError] = useState('поле Возраст не может быть  пустым')
+
+    const [isFormValid, setIsFormValid] = useState(false)
+
+    useEffect(() => {
+        if (nameError || fNameError || iAgeError) {
+            setIsFormValid(prevState => false)
+        } else {
+            setIsFormValid(prevState => true)
+        }
+    }, [nameError, fNameError, iAgeError])
 
     const nameHandler = (e) => {
         setName(e.target.value)
         const reg = /^([a-zа-яё]+[\s]{0,1}[a-zа-яё]+[\s]{0,1}[a-zа-яё]+)$/ig;
-        if (!reg.test(String(e.target.value))) {
+        if (!reg.test(e.target.value)) {
             setNameError('Неккоректное имя')
         } else {
-            setNameError('')
+            setNameError('');
+            setFriend(state => ({...state, firstname: e.target.value}));
         }
     }
 
+    const fNameHandler = (e) => {
+        setFName(e.target.value)
+        const reg = /^([a-zа-яё]+[\s]{0,1}[a-zа-яё]+[\s]{0,1}[a-zа-яё]+)$/ig;
+        if (!reg.test(e.target.value)) {
+            setFNameError('Неккоректное значение')
+        } else {
+            setFNameError('');
+            setFriend(state => ({...state, lastname: e.target.value}));
+        }
+    }
+
+    const iAgeHandler = (e) => {
+        setIAge(e.target.value)
+        const reg = /^[1-9][0-9]*$/;
+        if (!reg.test(e.target.value) || +(e.target.value) > 110) {
+            setIAgeError('Неккоректное значение')
+        } else {
+            setIAgeError('');
+            setFriend(state => ({...state, age: e.target.value}));
+        }
+    }
 
     const blurHandler = (e) => {
         switch (e.target.name) {
             case 'firstname':
-                setNameDirty(true)
+                setNameDirty(prevState => true)
                 break
             case 'lastname':
-                setFNameDirty(true)
+                setFNameDirty(prevState => true)
                 break
             case 'age':
-                setIAgeDirty(true)
+                setIAgeDirty(prevState => true)
                 break
         }
     }
@@ -61,10 +94,10 @@ export const AddFriend = props => {
             {(isNameDirty && nameError) && <div className = 'error'>{nameError}</div>}
             <input className="add-friends-input" onChange={e => nameHandler(e)} value={name} onBlur={e => blurHandler(e)} name = 'firstname' placeholder="Имя" />
             {(isFNameDirty && fNameError) && <div className = 'error'>{fNameError}</div>}
-            <input className="add-friends-input" value={fName} onBlur={e => blurHandler(e)} name = 'lastname' placeholder="Фамилия" onChange={(e) => setFriend(state => ({...state, lastname: e.target.value}))}/>
+            <input className="add-friends-input" onChange={e => fNameHandler(e)} value={fName} onBlur={e => blurHandler(e)} name = 'lastname' placeholder="Фамилия" />
             {(isIAgeDirty && iAgeError) && <div className = 'error'>{iAgeError}</div>}
-            <input className="add-friends-input" value={iAge} onBlur={e => blurHandler(e)} name = 'age' placeholder="Возраст" onChange={(e) => setFriend(state => ({...state, age: e.target.value}))}/>
-            <button className="add-friends-button" type="button" onClick={clickingOnAddFriendsButton}>Добавить!!</button>
+            <input className="add-friends-input" onChange={e => iAgeHandler(e)} value={iAge} onBlur={e => blurHandler(e)} name = 'age' placeholder="Возраст"/>
+            <button disabled={!isFormValid} className="add-friends-button" type="button" onClick={clickingOnAddFriendsButton}>Добавить!!</button>
         </form>
     )
 }
--- src/Components/EditInfoUser.js
+++ src/Components/EditInfoUser.js
@@ -1,36 +1,62 @@
 import React, {useState} from 'react';
+import {useEffect} from "react";
 import { IoClose } from "react-icons/io5";
 import { IoCheckmark } from "react-icons/io5";
 export const EditInfoUser = props => {
     const {nameItem, nameForm, showEditForm, onEdit} = props;
-    const [valueForm, setValueForm] = useState(
-        props.valueForm
-    )
+    // const [valueForm, setValueForm] = useState(
+    //     props.valueForm
+    // )
+
+    const [item, setItem] = useState(props.valueForm)
+    const [isItemDirty, setIsItemDirty] = useState('')
+    const [itemError, setItemError] = useState('')
+    const [isFormValid, setIsFormValid] = useState(false)
+
+    useEffect(() => {
+        if (itemError) {
+            setIsFormValid(prevState => false)
+        } else {
+            setIsFormValid(prevState => true)
+        }
+    }, [itemError])
+
+    const itemHandler = (e) => {
+        setItem(e.target.value)
+        const reg = /^([a-zа-яё]+[\s]{0,1}[a-zа-яё]+[\s]{0,1}[a-zа-яё]+)$/ig;
+        if (!reg.test(e.target.value)) {
+            setItemError('Неккоректное значение')
+        } else {
+            setItemError('');
+            setItem(e.target.value);
+        }
+    }
+
+    const blurHandler = (e) => {
+        setIsItemDirty(prevState => true)
+    }
 
     const clickingOnOkButton = () => {
-        onEdit({
-            nameForm: nameForm,
-            valueForm: valueForm
-        });
-        showEditForm();
+        if (isFormValid) {
+            onEdit({
+                nameForm: nameForm,
+                valueForm: item,
+            });
+            showEditForm();
+        }
     }
 
     const clickingOnCloseButton = () => {
         showEditForm();
     }
 
-
-    const changeEditForm = (e) => {
-        if (e.target.value !== '')
-            setValueForm(e.target.value)
-    }
-
     return (
         <form>
+            {(isItemDirty && itemError) && <div className = 'error'>{itemError}</div>}
             <input className="edit-form" placeholder={nameItem}
-                   onChange={changeEditForm}/>
+                   onChange={e => itemHandler(e)} value={item} onBlur={e => blurHandler(e)}/>
             <IoClose className="close-button" onClick={clickingOnCloseButton}/>
-            <IoCheckmark className="ok-button" onClick={clickingOnOkButton}/>
+            <IoCheckmark aria-disabled={!isFormValid} className="ok-button" onClick={clickingOnOkButton}/>
         </form>
     )
 }
